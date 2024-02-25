9ac2596551ba890e9c5c996df19d087efbf1945c
Nader Jemaa <nader.jomaa@yahoo.fr>
2024-02-25 18:34:20 +0100

    fixing docker file + cleanup

--- SV-CodingCase/SV-CodingCase.Extensions/Common/ResultStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SV_CodingCase.Extensions.Common
-{
-    public static class ResultStatus
-    {
-        public const string Success = "success";
-        public const string Failure = "failure";
-    }
-}
