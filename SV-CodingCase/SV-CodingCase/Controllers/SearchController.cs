c53e04d2c5547cc22f069344a83654edfe9e3f4c
Nader Jemaa <nader.jomaa@yahoo.fr>
2024-02-25 19:17:09 +0100

    changing the port number in FE for docker build

--- SV-CodingCase/SV-CodingCase/Controllers/SearchController.cs
+++ SV-CodingCase/SV-CodingCase/Controllers/SearchController.cs
@@ -1,18 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using SV_CodingCase.Domain.Application;
-using SV_CodingCase.Domain.Repositories;
 
 namespace SV_CodingCase.Controllers
 {
     [ApiController]
     public class SearchController : BaseApiController
     {
-        private readonly IDataRepository _dataRepository;
-
-        public SearchController(ILogger<SearchController> logger, IDataRepository dataRepository) : base(logger)
-        {
-            _dataRepository = dataRepository;
-        }
+        public SearchController(ILogger<SearchController> logger) : base(logger){}
 
         [HttpGet]
         public async Task<IActionResult> Get(string searchInput)
